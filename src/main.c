5ac84d0a9cdf7a0d9f647b7e8f8e885fe47400a6
zerg <anonzerg@proton.me>
2025-12-29 00:12:58 +0330

    add collision between particles.

--- src/main.c
+++ src/main.c
@@ -13,11 +13,12 @@ typedef struct
   Color color;
 } Particle;
 
-#define N 1000
+#define N 500
 
 void InitParticle (Particle *particle, Vector2 position, Vector2 velocity,
                    float radius, Color color);
 void CollisionWithWall (Particle *particle, int screenWidth, int screenHeight);
+void ImpulseCollision (Particle *particle1, Particle *particle2);
 
 int
 main (void)
@@ -31,25 +32,44 @@ main (void)
   r = gsl_rng_alloc (t);
 
   Particle ensemble[N];
-  int i;
+  int i, j;
 
   for (i = 0; i < N; i++)
     {
-      Vector2 position
-          = (Vector2){ gsl_ran_gaussian (r, 50.0) + (float)screenWidth / 2,
-                       gsl_ran_gaussian (r, 50.0) + (float)screenHeight / 2 };
+      bool validPosition = false;
+      Vector2 position;
+
+      while (!validPosition)
+        {
+          position = (Vector2){
+            gsl_ran_gaussian (r, 100.0) + (float)screenWidth / 2,
+            gsl_ran_gaussian (r, 100.0) + (float)screenHeight / 2
+          };
+          validPosition = true;
+          for (int j = 0; j < i; j++)
+            {
+              float dist = Vector2Distance (position, ensemble[j].position);
+              if (dist < 20.0f) // 8.0 + 8.0 radius sum
+                {
+                  validPosition = false;
+                  break;
+                }
+            }
+        }
+
       Vector2 velocity
-          = (Vector2){ gsl_ran_rayleigh (r, 5.0), gsl_ran_rayleigh (r, 5.0) };
+
+          = (Vector2){ gsl_ran_rayleigh (r, 1.0), gsl_ran_rayleigh (r, 1.0) };
       InitParticle (&ensemble[i], position, velocity, 8.0, RAYWHITE);
     }
 
   for (i = 0; i < N / 10; i++)
     {
-      ensemble[i].color = MAROON;
+      ensemble[i].color = GREEN;
     }
 
   InitWindow (screenWidth, screenHeight, "Gas Box");
-  SetTargetFPS (60);
+  SetTargetFPS (120);
 
   while (!WindowShouldClose ())
     {
@@ -60,7 +80,23 @@ main (void)
           ensemble[i].position
               = Vector2Add (ensemble[i].position, ensemble[i].velocity);
           CollisionWithWall (&ensemble[i], screenWidth, screenHeight);
+        }
 
+      for (i = 0; i < N; i++)
+        {
+          for (j = i + 1; j < N; j++)
+            {
+              float dist = Vector2Distance (ensemble[i].position,
+                                            ensemble[j].position);
+              if (dist < ensemble[i].radius + ensemble[j].radius)
+                {
+                  ImpulseCollision (&ensemble[i], &ensemble[j]);
+                }
+            }
+        }
+
+      for (i = 0; i < N; i++)
+        {
           DrawCircleV (ensemble[i].position, ensemble[i].radius,
                        ensemble[i].color);
         }
@@ -84,11 +120,46 @@ InitParticle (Particle *particle, Vector2 position, Vector2 velocity,
 void
 CollisionWithWall (Particle *particle, int screenWidth, int screenHeight)
 {
-  if (particle->position.x - particle->radius <= 0
-      || particle->position.x + particle->radius >= screenWidth)
-    particle->velocity.x = -1.0 * particle->velocity.x;
+  // left wall
+  if (particle->position.x - particle->radius <= 0)
+    {
+      particle->position.x = particle->radius;
+      particle->velocity.x = fabs (particle->velocity.x);
+    }
+  // right wall
+  else if (particle->position.x + particle->radius >= screenWidth)
+    {
+      particle->position.x = screenWidth - particle->radius;
+      particle->velocity.x = -fabs (particle->velocity.x);
+    }
+  // top wall
+  if (particle->position.y - particle->radius <= 0)
+    {
+      particle->position.y = particle->radius;
+      particle->velocity.y = fabs (particle->velocity.y);
+    }
+  // bottom wall
+  else if (particle->position.y + particle->radius >= screenHeight)
+    {
+      particle->position.y = screenHeight - particle->radius;
+      particle->velocity.y = -fabs (particle->velocity.y);
+    }
+}
 
-  if (particle->position.y - particle->radius <= 0
-      || particle->position.y + particle->radius >= screenHeight)
-    particle->velocity.y = -1.0 * particle->velocity.y;
+void
+ImpulseCollision (Particle *particle1, Particle *particle2)
+{
+  Vector2 normal = Vector2Normalize (
+      Vector2Subtract (particle2->position, particle1->position));
+  float relativeVelocity = Vector2DotProduct (
+      Vector2Subtract (particle1->velocity, particle2->velocity), normal);
+
+  if (relativeVelocity <= 0)
+    return;
+
+  float impulse = relativeVelocity;
+  Vector2 impulseVector = Vector2Scale (normal, impulse);
+
+  particle1->velocity = Vector2Subtract (particle1->velocity, impulseVector);
+  particle2->velocity = Vector2Add (particle2->velocity, impulseVector);
 }
